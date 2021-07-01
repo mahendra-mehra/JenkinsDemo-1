82218a0b9af912ce67170f2856e28c03d2fb4913
Mahendra Mehra <mahendra488@gmail.com>
2021-07-01 23:38:29 +0530

    new change00o

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
     public static void main(String[] args){
         for(int i=1;i<=10;i++){
-            System.out.println(+i+".Hello SlFIT!!");
+            System.out.println(+i+".Hello 66SlFIT!!");
         }
     }
 }
